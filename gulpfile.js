9875ff2b8280e510faec7d1497dad223e1cb3361
Tanmay Deshpande <deshpande@outlook.in>
2019-09-29 18:50:28 -0700

    Assignment 5
    Gulp and Sass

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var {src,dest,watch,series} = require('gulp');
+var cleanCSS = require('gulp-clean-css');
+var sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+
+function generateCSS(){
+    return src('src/*.scss')
+    .pipe(sass())     
+    .pipe(dest('dist/css'));
+    // .pipe(dest(minifyCSS()));    
+};
+
+function minifyCSS(){    
+    return src('dist/css/*.css')
+    .pipe(cleanCSS())     
+    .pipe(dest('dist/css')); 
+}
+
+function init(){
+    watch('src/*.scss',series(generateCSS,minifyCSS));            
+};
+
+
+
+exports.default = init;
